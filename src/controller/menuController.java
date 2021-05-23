bafb37f0600b1bc0e968f0cd1602b7655f233e9d
Daniel <dancostafsa@hotmail.com>
2021-05-23 17:12:41 -0300

    algumasTelas

---
+++ src/controller/menuController.java
@@ -0,0 +1,40 @@
+package controller;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+
+public class menuController {
+
+    @FXML
+    private Button paciencia;
+
+    @FXML
+    private Button bigbertha;
+
+    @FXML
+    private ImageView carta1;
+
+    @FXML
+    private ImageView carta2;
+
+    @FXML
+    private ImageView carta3;
+
+    @FXML
+    private ImageView carta4;
+
+    @FXML
+    void escolhaBigBertha(ActionEvent event) {
+
+    }
+
+    @FXML
+    void escolhaPaciencia(ActionEvent event) {
+
+    }
+
+}
---
+++ src/controller/movimentosController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class movimentosController {
+
+    @FXML
+    private Button botaoDescarte_Fileira;
+
+    @FXML
+    private Button botaoDescarte_Fundacao;
+
+    @FXML
+    private Button botaoFileira_Fundacao;
+
+    @FXML
+    private Button botaoFileira_Fileira;
+
+    @FXML
+    private Button botaoVoltar;
+
+    @FXML
+    void escolhaDescarteFundacao(ActionEvent event) {
+
+    }
+
+    @FXML
+    void escolhaDescarte_Fileira(ActionEvent event) {
+
+    }
+
+    @FXML
+    void escolhaFileira_Fileira(ActionEvent event) {
+
+    }
+
+    @FXML
+    void escolhaFileira_Fundacao(ActionEvent event) {
+
+    }
+
+    @FXML
+    void escolhaVoltar(ActionEvent event) {
+
+    }
+
+}
